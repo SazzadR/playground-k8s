4a0d0bb4c50c2fe6ecf202859bcd8d2f0262ff41
Sazzad <sazzad.rahman.bd@gmail.com>
2025-03-09 21:10:51 +0600

    Installed matrics-server, setup deployment, service and pod autoscaler for demo app

--- infrastructure/eks.tf
+++ infrastructure/eks.tf
@@ -113,3 +113,18 @@ resource "aws_eks_node_group" "general" {
         ignore_changes = [scaling_config[0].desired_size]
     }
 }
+
+resource "helm_release" "metrics_server" {
+    name = "metrics-server"
+
+    repository = "https://kubernetes-sigs.github.io/metrics-server/"
+    chart      = "metrics-server"
+    namespace  = "kube-system"
+    version    = "3.12.2"
+
+    values = [
+        file("${path.module}/values/metrics-server.yaml")
+    ]
+
+    depends_on = [aws_eks_node_group.general]
+}
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,9 +4,29 @@ terraform {
             source  = "hashicorp/aws"
             version = "~> 5.0"
         }
+        helm = {
+            source  = "hashicorp/helm"
+            version = "2.14.0"
+        }
     }
 }
 
 provider "aws" {
     region = local.region
+}
+
+data "aws_eks_cluster" "main" {
+    name = aws_eks_cluster.main.name
+}
+
+data "aws_eks_cluster_auth" "main" {
+    name = aws_eks_cluster.main.name
+}
+
+provider "helm" {
+    kubernetes {
+        host  = data.aws_eks_cluster.main.endpoint
+        token = data.aws_eks_cluster_auth.main.token
+        cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority.0.data)
+    }
 }
